5d0cc8f0f5d3dd4a66a6d7c4473edfed27129264
Austin Moran <moraustin@gmail.com>
2013-07-04 13:43:42 -0400

    cleanup, output nonce on write

--- src/native.server.cc
+++ src/native.server.cc
@@ -2,6 +2,7 @@
 #include <cstdlib>
 #include <cstring>
 
+#include <iostream>
 #include <string>
 
 #include <uv.h>
@@ -11,17 +12,17 @@
 namespace xblab {
 
 
+// TODO: this is crap
 struct DataBaton {
     DataBaton(){
         request.data = this;
         client.data = this;
-        write_req.data = this;
     }
     ~DataBaton(){}
     uv_tcp_t client;
     uv_work_t request;
-    uv_write_t write_req;
     std::string buf;
+    int buflen;
     std::string nonce;
     std::string err;
     void *auxData;
@@ -30,27 +31,34 @@ struct DataBaton {
 
 uv_loop_t *loop;
 
+
+// Defined extern in crypto.cc
 std::string publicKeyFile = "rsapub.pem";
 std::string privateKeyFile = "rsapriv.pem";
-std::string keyPassword = "aardvark";
+std::string keyPassword; // args
 
 
-uv_buf_t alloc_buffer(uv_handle_t *handle, size_t suggested_size) {
+// TODO: integrate into DataBaton
+uv_buf_t
+alloc_buffer(uv_handle_t *handle, size_t suggested_size) {
     return uv_buf_init((char *) malloc(suggested_size), suggested_size);
 }
 
-void echo_write(uv_write_t *req, int status) {
+
+// TODO: should really be called after_write
+void
+echo_write(uv_write_t *req, int status) {
     if (status == -1) {
         fprintf(stderr, "Write error %s\n", uv_err_name(uv_last_error(loop)));
     }
 
-    DataBaton *baton = reinterpret_cast<DataBaton *>(req->data);
-    char *base = (char*) baton->auxData;
-    free(base);
+    // req is temporary for each write request
     free(req);
 }
 
-void echo_read(uv_stream_t *client, ssize_t nread, uv_buf_t buf) {
+
+void
+echo_read(uv_stream_t *client, ssize_t nread, uv_buf_t buf) {
     if (nread == -1) {
         if (uv_last_error(loop).code != UV_EOF){
             fprintf(stderr, "Read error %s\n", uv_err_name(uv_last_error(loop)));
@@ -59,27 +67,30 @@ void echo_read(uv_stream_t *client, ssize_t nread, uv_buf_t buf) {
         return;
     }
 
-
-    // uv_write_t *req = (uv_write_t *) malloc(sizeof(uv_write_t));
     DataBaton *baton = reinterpret_cast<DataBaton *>(client->data);
 
-    // grab nonce
+    // test: generate nonce
     baton->nonce = Crypto::generateNonce();
+    uv_write_t *req = (uv_write_t *) malloc(sizeof(uv_write_t));
 
-    baton->auxData = buf.base;
-    buf.len = nread + sizeof(baton->nonce.data()); // might use + baton->nonce.size()
-    uv_write(&baton->write_req, client, &buf, 1, echo_write);
+    req->data = &baton->nonce[0];
+    buf.base = &baton->nonce[0];
+    buf.len = baton->nonce.size();
+    uv_write(req, client, &buf, 1, echo_write);
 }
 
-void on_new_connection(uv_stream_t *server, int status) {
+
+void
+on_new_connection(uv_stream_t *server, int status) {
     if (status == -1) {
         // error!
         return;
     }
 
     DataBaton *baton = new DataBaton();
-    // uv_tcp_t *client = (uv_tcp_t*) malloc(sizeof(uv_tcp_t));
     uv_tcp_init(loop, &baton->client);
+
+    // TODO: where does suggested size come from??
     if (uv_accept(server, (uv_stream_t*) &baton->client) == 0) {
         uv_read_start((uv_stream_t*) &baton->client, alloc_buffer, echo_read);
     }
@@ -91,12 +102,14 @@ void on_new_connection(uv_stream_t *server, int status) {
 } // namespace xblab
 
 
-int main(int argc, char** argv) {
-    if (argc != 2){
-        fprintf(stderr, "usage: %s port\n", argv[0]);
+int
+main(int argc, char** argv) {
+    if (argc != 3){
+        fprintf(stderr, "usage: %s port, passphrase\n", argv[0]);
         return 1;
     }
     int port = atoi(argv[1]);
+    xblab::keyPassword = std::string(argv[2]);
 
     xblab::loop = uv_default_loop();
     uv_tcp_t server;
